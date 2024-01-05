796192369463e8eaf7b91d1dfb494f829afd0be9
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-01-05 15:10:42 +0500

    Update app.js

--- Advance JS/class 07/app.js
+++ Advance JS/class 07/app.js
@@ -3,7 +3,7 @@ console.log("hello class")
 const promise = new Promise((resolve, reject) => {
     const age = 20         
         
-    if (age > 18) {
+    if (age > 18) {         
         resolve("promise solve")
     } else {
         reject("promise reject")
